934cef94f2c5755220d2eb8ff8cba173ec4897de
Jeffrey Castro <castrojeffreym@oaxis.com>
2018-09-25 21:54:36 +0800

    Add allow_blank true option for email format validation of User
    - to prevent invalid format message when email is blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates_presence_of :email
   validates_uniqueness_of :email, case_sensitive: false
-  validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, message: "has invalid format"
+  validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, allow_blank: true, message: "has invalid format"
 end
